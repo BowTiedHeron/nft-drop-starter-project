b24ffe473fa1164e0fe31fdc0cef774363c56510
Dash <dash@Dashs-MacBook-Pro.local>
2021-12-05 11:14:27 +0100

    test

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-
+//testtesttest
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
